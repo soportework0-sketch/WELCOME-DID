8a851a7c58cd86557578da95552e28141ab1a258
soportework0-sketch <soportework0@gmail.com>
2026-08-09 22:24:33 -0500

    packjeson.js

---
+++ packjeson.js
@@ -0,0 +1,12 @@
+{
+  "name": "did-bot",
+  "version": "1.0.0",
+  "description": "Bot de bienvenida de la DID",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.21.0"
+  }
+}
